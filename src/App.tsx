162b37baa566825e543e2d113ee394074a2603c4
Suzy-Lee <mulddang2@naver.com>
2023-10-27 22:34:43 +0900

    feat: 입력한 데이터 playListTable에 추가하는 기능

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import GlobalStyles from './GlobalStyles ';
 import styled from 'styled-components';
-import AddList from './components/AddList';
-import ViewList from './components/ViewList';
+import AddListForm from './components/AddListForm';
+import PlayListTable from './components/PlayListTable';
+import { useState } from 'react';
+import { IBasePlayList, IPlayList } from './interface';
 
 const Layout = styled.div`
   width: 100%;
@@ -23,7 +25,7 @@ const ListContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
   margin-left: -1rem;
-    margin-right: -1rem;
+  margin-right: -1rem;
 
   > div {
     padding: 0 1rem;
@@ -31,14 +33,42 @@ const ListContainer = styled.div`
 `;
 
 const App = (): JSX.Element => {
+  // NOTE: Data
+  const playListData: Array<IPlayList> = [
+    {
+      id: 1,
+      title: `Pfitzner: Sieben Lieder, Op. 2 - II. Ist der Himmel darum im Lenz so blau(with Matthias Goerne) 7개의 가곡집 중 '왜 봄에 하늘은 그렇게 푸르게 빛나는 걸까요?`,
+      artist: '조성진',
+    },
+    {
+      id: 2,
+      title: 'Chopin: Scherzo No. 1 in B Minor, Op. 20',
+      artist: '임동혁',
+    },
+    {
+      id: 3,
+      title: 'William Bolcom: Graceful Ghost Rag',
+      artist: '양인모',
+    },
+  ];
+
+  // const initialFormState: IUser = { title: '', artist: '', id: null };
+
+  const [lists, setLists] = useState(playListData);
+
+  const addList = (newList: IBasePlayList) => {
+    const id = lists.length + 1;
+    setLists([...lists, { ...newList, id }]);
+  };
+
   return (
     <>
       <GlobalStyles />
       <Layout>
         <h1>CRUD App with Hooks</h1>
         <ListContainer>
-          <AddList />
-          <ViewList />
+          <AddListForm onAddList={addList} />
+          <PlayListTable playLists={lists} />
         </ListContainer>
       </Layout>
     </>
---
+++ src/components/PlayListTable.tsx
@@ -0,0 +1,99 @@
+import styled from 'styled-components';
+import { BsPencilFill } from 'react-icons/bs';
+import { BsFillEraserFill } from 'react-icons/bs';
+import { IPlayList } from '../interface';
+
+interface IProps {
+  playLists: Array<IPlayList>;
+}
+
+const ViewListContainer = styled.div`
+  @media (min-width: 1000px) {
+    flex: 1;
+    margin-bottom: 0;
+  }
+
+  h2 {
+    font-size: 1.5rem;
+    margin: 1.5rem 0;
+
+    @media (min-width: 600px) {
+      font-size: 2rem;
+    }
+  }
+`;
+
+const Table = styled.table`
+  width: 100%;
+  max-width: 100%;
+  border-collapse: collapse;
+
+  th,
+  td {
+    text-align: left;
+    padding: 0.5rem;
+    width: 33.3%;
+  }
+
+  tr {
+    border-bottom: 1px solid #dedede;
+  }
+
+  thead th {
+    border-bottom: 2px solid #dedede;
+  }
+
+  button {
+    display: inline-block;
+    background: transparent;
+    border: 1px solid #cdcdcd;
+    color: #4e4e4e;
+    margin-right: 0.5rem;
+
+    &:hover {
+      color: #4e4e4e;
+      border: 1px solid #818181;
+    }
+  }
+`;
+
+const PlayListTable: React.FunctionComponent<IProps> = (props) => {
+  return (
+    <ViewListContainer>
+      <h2>View My 2023 Music Playlist</h2>
+      <Table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Artist</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.playLists.length > 0 ? (
+            props.playLists.map((i) => (
+              <tr key={i.id}>
+                <td>{i.title}</td>
+                <td>{i.artist}</td>
+                <td className='actions'>
+                  <button>
+                    <BsPencilFill />
+                  </button>
+                  <button>
+                    <BsFillEraserFill />
+                  </button>
+                </td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan={3}>No users</td>
+            </tr>
+          )}
+        </tbody>
+      </Table>
+    </ViewListContainer>
+  );
+};
+
+export default PlayListTable;
