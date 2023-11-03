d6cf42b35b63d55e9ffbee69d2841ae075bf4760
Suzy-Lee <mulddang2@naver.com>
2023-11-04 01:02:34 +0900

    feat: styledtheme 타입 추가

---
+++ src/styles/styled.d.ts
@@ -0,0 +1,14 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    bgColor: string;
+    textColor: string;
+    borderColor: string;
+    buttonColor: string;
+    iconBorderColor: string;
+    iconColor: string;
+    shadowColor: string;
+    iconHoverBgColor: string;
+  }
+}
---
+++ src/styles/styledTheme.ts
@@ -0,0 +1,28 @@
+export const lightTheme = {
+  bgColor: '#ffffff',
+  textColor: '#212529',
+  borderColor: '#DEDEDE',
+  buttonColor: '#570DF8',
+  iconBorderColor: '#CDCDCD',
+  iconColor: '#4E4E4E',
+  iconHoverBorderColor: '#818181',
+  shadowColor: '#b39ce4',
+  iconHoverBgColor: '#0000000D',
+};
+
+export const darkTheme = {
+  bgColor: '#212529',
+  textColor: '#FFFFFF',
+  borderColor: '#585858',
+  buttonColor: '#661AE6',
+  iconBorderColor: '#6A6A6A',
+  iconColor: '#ECECEC',
+  iconHoverBorderColor: '#ffffff',
+  shadowColor: '#9768fc',
+  iconHoverBgColor: '#FFFFFF1A',
+};
+
+export const theme = {
+  darkTheme,
+  lightTheme,
+};
